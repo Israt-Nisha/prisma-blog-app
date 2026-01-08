85a353aadf67ccd773e462c9b231ebda12173faf
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-08 21:39:41 +0600

    create comment api

--- src/modules/post/comment/comment.router.ts
+++ src/modules/post/comment/comment.router.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import auth, { UserRole } from "../../../middlewares/auth";
+import { CommentController } from "./comment.controller";
+
 const router = express.Router();
 
+
+router.post(
+    "/",
+    auth(UserRole.USER, UserRole.ADMIN),
+    CommentController.createComment
+)
 
 export const commentRouter = router;
